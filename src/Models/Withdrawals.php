43d0cefa4b4150f6694771314be4176ded1ef774
Guilherme Assis <guilherme.assis@codificar.com.br>
2020-07-16 14:07:09 -0300

    correcao model

--- src/Models/Withdrawals.php
+++ src/Models/Withdrawals.php
@@ -31,7 +31,7 @@ class Withdrawals extends Eloquent
     public static function getWithdrawalsSummary($ledgerId = null, $enviroment)
     {
 
-        $withdrawalsSummary = Finance::select('finance.id as id', 'finance.value as value', 'finance.compensation_date as date', 'bank.name as bank', 'ledger_bank_account.account as bankAccount')
+        $withdrawalsSummary = Finance::select('finance.id as id', 'finance.value as formattedValue', 'finance.compensation_date as date', 'bank.name as bank', 'ledger_bank_account.account as bankAccount')
                                 ->join('ledger', 'finance.ledger_id', '=', 'ledger.id')
                                 ->join($enviroment, 'ledger.'.$enviroment.'_id', '=', $enviroment.'.id')
                                 ->join('ledger_bank_account', 'finance.ledger_bank_account_id', 'ledger_bank_account.id')
@@ -40,8 +40,10 @@ class Withdrawals extends Eloquent
                                 ->where('finance.reason', '=', 'WITHDRAW')
                                 ->orderBy('finance.id', 'desc')
                                 ->get();
-      
         
+        foreach($withdrawalsSummary as $withdral) {
+            $withdral->formattedValue = currency_format($withdral->formattedValue);
+        }
         return $withdrawalsSummary;
     }
 }
