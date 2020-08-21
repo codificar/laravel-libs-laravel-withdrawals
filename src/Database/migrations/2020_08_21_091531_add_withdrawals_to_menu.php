dc04d159140d56f7c5277bbc3833950d85a7103f
Guilherme Assis <guilherme.assis@codificar.com.br>
2020-08-21 11:26:38 -0300

    migration

--- src/Database/migrations/2020_08_21_091531_add_withdrawals_to_menu.php
+++ src/Database/migrations/2020_08_21_091531_add_withdrawals_to_menu.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-use Codificar\Withdrawals\Database\seeds\AddWithdrawalsMenu;
 
 class AddWithdrawalsToMenu extends Migration
 {
@@ -15,9 +14,12 @@ class AddWithdrawalsToMenu extends Migration
      */
     public function up()
     {
-        Artisan::call('db:seed', [
-        '--class' => AddWithdrawalsMenu::class,
-        ]);
+        $withdrawalsReport = Permission::updateOrCreate(['name' => 'Withdrawals Report'] , ['name' => 'Withdrawals Report', 'parent_id' => 2316, 'order' => 915, 'is_menu' => 1, 'url' => '/admin/libs/withdrawals', 'icon' => 'mdi mdi-file-chart']);
+
+        $cnabSettings = Permission::updateOrCreate(['name' => 'Cnab Settings'] , ['name' => 'Cnab Settings', 'parent_id' => 2319, 'order' => 915, 'is_menu' => 1, 'url' => '/admin/libs/cnab_settings', 'icon' => 'mdi mdi-bank']);
+
+        ProfilePermission::updateOrCreate(['permission_id' => $withdrawalsReport->id], ['profile_id' => 3, 'permission_id' => $withdrawalsReport->id]);
+        ProfilePermission::updateOrCreate(['permission_id' => $cnabSettings->id], ['profile_id' => 3, 'permission_id' => $cnabSettings->id]);
     }
 
     /**
