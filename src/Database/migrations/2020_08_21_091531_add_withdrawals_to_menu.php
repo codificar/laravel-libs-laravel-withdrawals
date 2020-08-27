24ce1f53147416ba1628e074553f7c2544ffb514
Guilherme Assis <guilherme.assis@codificar.com.br>
2020-08-27 12:46:17 -0300

    migration menu

--- src/Database/migrations/2020_08_21_091531_add_withdrawals_to_menu.php
+++ src/Database/migrations/2020_08_21_091531_add_withdrawals_to_menu.php
@@ -14,10 +14,15 @@ class AddWithdrawalsToMenu extends Migration
      */
     public function up()
     {
-        $withdrawalsReport = Permission::updateOrCreate(['name' => 'Withdrawals Report'] , ['name' => 'Withdrawals Report', 'parent_id' => 2316, 'order' => 915, 'is_menu' => 1, 'url' => '/admin/libs/withdrawals', 'icon' => 'mdi mdi-file-chart']);
-
+        //create the Withdrawals report menu
+        $withdrawalsReport = Permission::updateOrCreate(['name' => 'Withdrawals Report'] , ['name' => 'Withdrawals Report', 'parent_id' => 2316, 'order' => 915, 'is_menu' => 1, 'url' => '/admin/libs/withdrawals']);
+        //Add the icon
+        DB::statement("UPDATE `permission` SET `icon`='mdi mdi-file-chart' WHERE `id` = $withdrawalsReport->id ;");
+        
+        //create the cnab settings in menu
         $cnabSettings = Permission::updateOrCreate(['name' => 'Cnab Settings'] , ['name' => 'Cnab Settings', 'parent_id' => 2319, 'order' => 915, 'is_menu' => 1, 'url' => '/admin/libs/cnab_settings', 'icon' => 'mdi mdi-bank']);
-
+       //Add the icon
+        DB::statement("UPDATE `permission` SET `icon`='mdi mdi-bank' WHERE `id` = $cnabSettings->id ;");
     }
 
     /**
