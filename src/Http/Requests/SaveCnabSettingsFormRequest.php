a0c14101df4be045defd9620f42a9c12f4963192
Guilherme Assis <guilherme.assis@codificar.com.br>
2020-12-22 09:18:33 -0300

    traducoes

--- src/Http/Requests/SaveCnabSettingsFormRequest.php
+++ src/Http/Requests/SaveCnabSettingsFormRequest.php
@@ -57,27 +57,27 @@ class SaveCnabSettingsFormRequest extends FormRequest {
     public function messages()
     {
         return [
-            'settings.rem_company_name'     => 'rem_company_name is required',
-            'settings.rem_cpf_or_cnpj'      => 'rem_cpf_or_cnpj  is required',
-            'settings.rem_document'         => 'rem_document is required',
-            'settings.rem_agency'           => 'rem_agency is required',
-            'settings.rem_agency_dv'        => 'rem_agency_dv is required',
-            'settings.rem_account'          => 'rem_account is required',
-            'settings.rem_account_dv'       => 'rem_account_dv is required',
-            'settings.rem_bank_code'        => 'rem_bank_code is required',
-            'settings.rem_agreement_number' => 'rem_agreement_number is required',
-            'settings.rem_transfer_type'    => 'rem_transfer_type is required',
+            'settings.rem_company_name.required'        => 'O campo nome da empresa é necessário',
+            'settings.rem_cpf_or_cnpj.required'         => 'O campo tipo de documento é necessário',
+            'settings.rem_document.required'            => 'O campo documento é necessário',
+            'settings.rem_agency.required'              => 'O campo agência é necessário',
+            'settings.rem_agency_dv.required'           => 'O campo digito da agência é necessário',
+            'settings.rem_account.required'             => 'O campo conta é necessário',
+            'settings.rem_account_dv.required'          => 'O campo dígito da conta é necessário',
+            'settings.rem_bank_code.required'           => 'O campo código do banco é necessário',
+            'settings.rem_agreement_number.required'    => 'O campo convênio é necessário',
+            'settings.rem_transfer_type.required'       => 'O campo tipo de transferência é necessário',
 
-            'settings.rem_environment'      => 'rem_environment is required',
-            'settings.rem_address'          => 'rem_address is required',
-            'settings.rem_address_number'   => 'rem_address_number is required',
-            'settings.rem_city'             => 'rem_city is required',
-            'settings.rem_cep'              => 'rem_cep is required',
-            'settings.rem_state'            => 'rem_state is required',
-            'settings.rem_operation'        => 'rem_operation is string',
-            'settings.rem_type_compromise'  => 'rem_type_compromise is string',
-            'settings.rem_code_compromise'  => 'rem_code_compromise is string',
-            'settings.rem_param_transmission'=> 'rem_param_transmission is string'
+            'settings.rem_environment.required'         => 'O campo ambiente é necessário',
+            'settings.rem_address.required'             => 'O campo endereço é necessário',
+            'settings.rem_address_number.required'      => 'O campo número do endereço é necessário',
+            'settings.rem_city.required'                => 'O campo cidade é necessário',
+            'settings.rem_cep.required'                 => 'O campo cep é necessário',
+            'settings.rem_state.required'               => 'O campo estado é necessário',
+            'settings.rem_operation.required'           => 'O campo operação é necessário',
+            'settings.rem_type_compromise.required'     => 'O campo tipo de compromisso é necessário',
+            'settings.rem_code_compromise.required'     => 'O campo código do compromisso é necessário',
+            'settings.rem_param_transmission.required'  => 'O campo parâmetro de transmissão é necessário'
         ];
     }
 
